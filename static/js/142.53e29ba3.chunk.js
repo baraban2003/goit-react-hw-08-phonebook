6fc65aa83a75234381506911fefde6a8346e9f66
Volodymyr Baraban <barabanv2003@gmail.com>
2022-06-14 22:23:24 +0300

    Updates

---
+++ static/js/142.53e29ba3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[142],{7142:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(184);function u(){return(0,r.jsx)("h1",{children:"404 Not found page :("})}}}]);
+//# sourceMappingURL=142.53e29ba3.chunk.js.map
